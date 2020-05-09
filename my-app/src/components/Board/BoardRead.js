95e0f49101392c159cc88863b22513a042db0f2c
mcpeleeGit <mcpelee@gmail.com>
2020-05-09 11:38:25 +0900

    board publishing

--- my-app/src/components/Board/BoardRead.js
+++ my-app/src/components/Board/BoardRead.js
@@ -11,8 +11,13 @@ export class BoardRead extends Component {
                 <ListGroup>
                     <ListGroup.Item>
 React-Bootstrap replaces the Bootstrap JavaScript. Each component has been built from scratch as a true React component, without unneeded dependencies like jQuery.
-
 As one of the oldest React libraries, React-Bootstrap has evolved and grown alongside React, making it an excellent choice as your UI foundation.
+
+Built with compatibility in mind, we embrace our bootstrap core and strive to be compatible with the world's largest UI ecosystem.
+By relying entirely on the Bootstrap stylesheet, React-Bootstrap just works with the thousands of Bootstrap themes you already love.
+
+The React component model gives us more control over form and function of each component.
+Each component is implemented with accessibility in mind. The result is a set of accessible-by-default components, over what is possible from plain Bootstrap.
                     </ListGroup.Item>
                 </ListGroup>
                 <p />
